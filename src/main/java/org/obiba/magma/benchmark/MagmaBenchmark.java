7278b1a388004bf18babf6aa90954a31925eb132
Cedric Thiebault <cedric.thiebault@gmail.com>
2014-01-15 14:24:28 -0500

    Configure benchmarks

--- src/main/java/org/obiba/magma/benchmark/MagmaBenchmark.java
+++ src/main/java/org/obiba/magma/benchmark/MagmaBenchmark.java
@@ -37,7 +37,7 @@ public class MagmaBenchmark {
   private MagmaBenchmark() {}
 
   public static void main(String... args) throws Exception {
-    ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("/context.xml");
+    ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("/benchmark-context.xml");
     applicationContext.registerShutdownHook();
     applicationContext.getBean(MagmaBenchmark.class).runBenchmarks();
   }
